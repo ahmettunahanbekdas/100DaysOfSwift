17ae00b9eb42ed23384eec9e809424335af03312
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-10-25 17:27:24 +0300

    complete changeelon

--- ChangeElon/ChangeElon/ViewController.swift
+++ ChangeElon/ChangeElon/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelText: UILabel!
+    
+    var isImageDisplay = true
+    var isLabelDisplay = true
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func changeClick(_ sender: Any) {
+        
+        if isImageDisplay {
+            imageView.image = UIImage(named: "sadElon")
+        }else{
+            imageView.image = UIImage(named: "happyElon")
+        }
+        isImageDisplay = !isImageDisplay
+        
+        
+        if isImageDisplay {
+            labelText.text = "Elon is Happy"
+        } else {
+            labelText.text = "Elon is Sad"
+        }
+        isLabelDisplay = !isImageDisplay
+    }
+    
 }
-
