014f3491620112aab7363650ff1efcbf177d1aac
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-10-27 09:23:45 +0300

    create project-3 layoutproject

---
+++ Project 3- ProjectLayout/Project 3- ProjectLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 3- ProjectLayout
+//
+//  Created by Ahmet Tunahan Bekdaş on 27.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
