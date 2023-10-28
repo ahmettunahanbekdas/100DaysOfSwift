07d1bf8495d806368f6d8ff031dd8cf1a4da7fb7
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-10-28 15:03:22 +0300

    added user defaults

--- Project 5- BirthdayNoteTaker/Project 5- BirthdayNoteTaker/ViewController.swift
+++ Project 5- BirthdayNoteTaker/Project 5- BirthdayNoteTaker/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let newName = storedName as? String {
+            nameLabel.text = "Name: \(newName)"
+        }
+        
+        if let newBirthday = storedBirthday as? String {
+            birthdayLabel.text = "Surname: \(newBirthday)"
+        }
     }
 
+    @IBAction func saveButton(_ sender: Any) {
+        
+        UserDefaults.standard.set(nameTextField.text, forKey: "name")
+        UserDefaults.standard.set(birthdayTextField.text, forKey: "birthday")
 
+        nameLabel.text = "Name: \(nameTextField.text!)"
+        birthdayLabel.text = "Birthday: \(birthdayTextField.text!)"
+    }
+    
+    
+    @IBAction func deleteButton(_ sender: Any) {
+        
+        UserDefaults.standard.removeObject(forKey: "name")
+        UserDefaults.standard.removeObject(forKey: "birthday")
+        
+        nameLabel.text = "Name: "
+        birthdayLabel.text = "Birthday: "
+
+    }
+    
+    
 }
 
