744afc8b6ee374048238a4100613fdfc471e9e14
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-10-28 18:08:14 +0300

    create segueapp

---
+++ Project 6- SegueApp/Project 6- SegueApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Project 6- SegueApp
+//
+//  Created by Ahmet Tunahan Bekdaş on 28.10.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var secondViewControllerLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
---
+++ Project 6- SegueApp/Project 6- SegueApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Project 6- SegueApp
+//
+//  Created by Ahmet Tunahan Bekdaş on 28.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var viewControllerLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
+ 
