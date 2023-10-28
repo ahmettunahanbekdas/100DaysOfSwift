31764e07bf09ca8386ea72aa2d81f5f0282c6f66
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-10-28 12:31:14 +0300

    create birthdaytaker

---
+++ Project 5- BirthdayNoteTaker/Project 5- BirthdayNoteTaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 5- BirthdayNoteTaker
+//
+//  Created by Ahmet Tunahan Bekdaş on 28.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
