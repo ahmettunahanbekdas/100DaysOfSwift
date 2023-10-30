e834bd3a06efea59219fdab90c70118f375988bb
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-10-30 22:52:18 +0300

    username control

--- Project 7- AlertProject/Project  7- AlertProject/ViewController.swift
+++ Project 7- AlertProject/Project  7- AlertProject/ViewController.swift
@@ -24,21 +24,30 @@ class ViewController: UIViewController {
     
     @IBAction func signupButton(_ sender: Any) {
         // Kullanıcının girdiği şifreleri al
+        
+        // Kullanıcının girdiği kullanıcı adı, şifreleri al
+        let username = usernameTextField.text
         let password1 = passwordTextField.text
         let password2 = passwordagainTextField.text
-        
-        // Şifrelerin aynı olup olmadığını kontrol et
-        if password1 == password2 {
-            userName = usernameTextField.text ?? "İsim boş geçildi"
+
+        // Kullanıcı adı boş mu diye kontrol et
+        if username?.isEmpty == true {
+            let usernameAlert = UIAlertController(title: "Hata", message: "Kullanıcı adı boş geçilemez.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Tamam", style: .cancel, handler: nil)
+            usernameAlert.addAction(okAction)
+            self.present(usernameAlert, animated: true)
+        } else if password1 == password2 {
+            // Kullanıcı adı boş değilse ve şifreler aynıysa işlem devam edebilir
+            userName = username ?? "İsim boş geçildi"
             performSegue(withIdentifier: "toSecondVC", sender: nil)
-            
         } else {
-            // Şifreler aynı değilse uyarı göster
+            // Kullanıcı adı boş değilse ama şifreler aynı değilse uyarı göster
             let alert = UIAlertController(title: "Hata", message: "Şifreler uyuşmuyor. Lütfen aynı şifreyi giriniz.", preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "Tamam", style: .default, handler: nil)
+            let okAction = UIAlertAction(title: "Tamam", style: .cancel, handler:  nil)
             alert.addAction(okAction)
             self.present(alert, animated: true)
         }
+           
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
