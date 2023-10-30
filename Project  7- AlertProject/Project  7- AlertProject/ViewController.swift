75b9f9a9c64e55394a06cd09801148d587c2ba9c
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-10-30 22:04:42 +0300

    add buton and label

--- Project  7- AlertProject/Project  7- AlertProject/ViewController.swift
+++ Project  7- AlertProject/Project  7- AlertProject/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordagainTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func signupButton(_ sender: Any) {
+        
+    }
 }
 
