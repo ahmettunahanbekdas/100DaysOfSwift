aca192d2608408bfa219467dbdd8a3bc0e175cc7
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-10-31 10:10:02 +0300

    add timer func

---
+++ Project 9- Timer Project/Project 9- Timer Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 9- Timer Project
+//
+//  Created by Ahmet Tunahan Bekdaş on 31.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var timer = Timer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
