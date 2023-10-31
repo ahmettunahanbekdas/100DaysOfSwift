55fd28b73853c98c8c7c7fee441418a95ae6cb3f
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-10-31 10:07:11 +0300

    gesurereconizer and image touch

--- Project 8- GestureRecognizerApp/Project 8- GestureRecognizerApp/ViewController.swift
+++ Project 8- GestureRecognizerApp/Project 8- GestureRecognizerApp/ViewController.swift
@@ -8,16 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var gatesImage: UIImageView!
     @IBOutlet weak var gatesLabel: UILabel!
+    
+    var imageControl = true
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeImage))
+        gatesImage.isUserInteractionEnabled = true
+        gatesImage.addGestureRecognizer(gestureRecognizer)
     }
-
-
+    
+    @objc func changeImage() {
+        if imageControl {
+            gatesImage.image = UIImage(named: "sad")
+            gatesLabel.text = "Bill is sad"
+        } else {
+            gatesImage.image = UIImage(named: "happy")
+            gatesLabel.text = "Bill is happy"
+        }
+        imageControl.toggle()
+    }
+    
 }
 
