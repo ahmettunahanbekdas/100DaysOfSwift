de9fda0cc680a38a37abb0d13570dc62132ffbd2
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-10-31 09:41:36 +0300

    added button and image

--- Project 8- GestureRecognizerApp/Project 8- GestureRecognizerApp/ViewController.swift
+++ Project 8- GestureRecognizerApp/Project 8- GestureRecognizerApp/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gatesImage: UIImageView!
+    @IBOutlet weak var gatesLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
