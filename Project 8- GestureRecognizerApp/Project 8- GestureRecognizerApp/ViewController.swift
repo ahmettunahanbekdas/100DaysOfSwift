84cedff3a21c5d29190b23f9cbda91249252be9c
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-10-31 09:33:35 +0300

    create project-8

---
+++ Project 8- GestureRecognizerApp/Project 8- GestureRecognizerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 8- GestureRecognizerApp
+//
+//  Created by Ahmet Tunahan Bekdaş on 31.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
