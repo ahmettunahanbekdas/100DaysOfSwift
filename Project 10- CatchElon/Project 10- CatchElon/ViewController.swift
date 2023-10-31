44f7d0d23c5be01fa51eb3c5e73f521c3312e54b
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-10-31 13:01:24 +0300

    create model

---
+++ Project 10- CatchElon/Project 10- CatchElon/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Project 10- CatchElon
+//
+//  Created by Ahmet Tunahan Bekdaş on 31.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var soccerLabel: UILabel!
+    
+    @IBOutlet weak var elon1: UIImageView!
+    @IBOutlet weak var elon2: UIImageView!
+    @IBOutlet weak var elon3: UIImageView!
+    @IBOutlet weak var elon4: UIImageView!
+    @IBOutlet weak var elon5: UIImageView!
+    @IBOutlet weak var elon6: UIImageView!
+    @IBOutlet weak var elon7: UIImageView!
+    @IBOutlet weak var elon8: UIImageView!
+    @IBOutlet weak var elon9: UIImageView!
+    
+    
+    @IBOutlet weak var highSoccerLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
