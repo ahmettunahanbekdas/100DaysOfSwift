417a368748f3a0c2c34f17a81a7cbf59c932350e
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-11-02 22:58:09 +0300

    tableview

--- LandmarkBook/LandmarkBook/ViewController.swift
+++ LandmarkBook/LandmarkBook/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+// ViewController adında bir sınıf tanımlanıyor.
+// UIViewController, UITableViewDelegate ve UITableViewDataSource protokollerini uyguluyor.
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // Storyboard'da bağlanmış bir UITableView öğesini temsil eden bir outlet.
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Uygulama başladığında çalışan fonksiyon.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Tablo görünümünün delegesini ve veri kaynağını bu sınıfa ayarlar.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+    
+    // Tablo görünümünde kaç satır gösterileceğini belirleyen fonksiyon.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30 // Şu an için her zaman 30 satır döndürülüyor.
+    }
+    
+    // Her hücrenin içeriğini ve görünümünü yapılandıran fonksiyon.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell() // Bir UITableViewCell örneği oluşturuluyor.
 
-
+        // Hücrenin metin içeriğini özelleştiriyoruz.
+        var content = cell.defaultContentConfiguration()
+ //     cell.textLabel?.text = "Text V1"
+        content.text = "Main Text" // Ana metin
+        content.secondaryText = "Secondary Text" // İkincil metin
+        cell.contentConfiguration = content
+        
+        return cell // Hücre döndürülüyor.
+    }
 }
 
