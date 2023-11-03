0d624b4c041343a016b22e6ae9465660cab0dca5
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-11-03 12:34:46 +0300

    delete func and alert

--- Project 11- HistorianBook /LandmarkBook/ViewController.swift
+++ Project 11- HistorianBook /LandmarkBook/ViewController.swift
@@ -23,13 +23,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
         
+        historians.append("Zeli Velidi Togan")
+        historians.append("Ali Reşad")
         historians.append("Halil İnalcık")
         historians.append("İlber Ortaylı")
+        historians.append("Reha Oğuz Türkkan")
         historians.append("Murat Bardakçı")
+        historians.append("Erhan Afyoncu")
         
+
+        
+        historiansImages.append(UIImage(named: "zeliveliditogan")!)
+        historiansImages.append(UIImage(named: "aliresad")!)
         historiansImages.append(UIImage(named: "halilinalcık")!)
         historiansImages.append(UIImage(named: "ilberortaylı")!)
+        historiansImages.append(UIImage(named: "rehaoguzturkkan")!)
         historiansImages.append(UIImage(named: "muratbardakcı")!)
+        historiansImages.append(UIImage(named: "erhanafyoncu")!)
+
         
         
     }
@@ -47,12 +58,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
-    
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         chosenName = historians[indexPath.row]
         chosenImage = historiansImages[indexPath.row]
         performSegue(withIdentifier: "toSecondVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            presentDeletionFailsafe(indexPath: indexPath)
+        }
+    }
+    
+    // delete cell
+    func presentDeletionFailsafe(indexPath: IndexPath) {
+        let alert = UIAlertController(title: nil, message: "Are you sure you'd like to delete \(historians[indexPath.row])", preferredStyle: .alert)
+
+        // yes action
+        let yesAction = UIAlertAction(title: "Yes", style: .default) { _ in
+            // replace data variable with your own data array
+            self.historians.remove(at: indexPath.row)
+            self.historiansImages.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+
+        alert.addAction(yesAction)
+
+        // cancel action
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -63,5 +97,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             
         }
     }
-}
+
+    }
+    
+ 
 
