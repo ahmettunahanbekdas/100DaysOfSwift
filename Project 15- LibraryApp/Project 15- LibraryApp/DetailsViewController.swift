18aa18cc0a2de38fb6ac93493088a1debf7bd70a
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-11-08 00:04:10 +0300

    ww

--- Project 15- LibraryApp/Project 15- LibraryApp/DetailsViewController.swift
+++ Project 15- LibraryApp/Project 15- LibraryApp/DetailsViewController.swift
@@ -1,15 +1,10 @@
-//
-//  DetailsViewController.swift
-//  Project 15- LibraryApp
-//
-//  Created by Ahmet Tunahan Bekdaş on 6.11.2023.
-//
-
 import UIKit
 import CoreData
 
 class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // Bu sınıf, kitap detaylarını ve bir kitap eklemek için kullanılan resim seçme işlevlerini içerir.
     
+    // MARK: - IBOutlets
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var writerText: UITextField!
@@ -18,67 +13,77 @@ class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-// MARK: - Recognizer
-        // Klavye Kapamak alanı
+        // MARK: - Recognizer
+        // Klavye kapatma alanı için bir jest tanımlar
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        view.addGestureRecognizer(tap) // ekrana ekledik
+        view.addGestureRecognizer(tap)
         
-        // Resime tıklama alanı
-        imageView.isUserInteractionEnabled = true // resmi tıklanabilir yaptık
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(addPhoto)) // tıklandığında çalışıcak fonksiyonu selector içinde belirttik
-           imageView.addGestureRecognizer(tapGestureRecognizer) // tıklama olayını resme ekledik
+        // Resme tıklama alanı için bir jest tanımlar
+        imageView.isUserInteractionEnabled = true
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(addPhoto))
+        imageView.addGestureRecognizer(tapGestureRecognizer)
     }
     
     // MARK: - Functions
     
-    // selector ile klavye kapama fonksiyonumuz
+    // Klavyeyi kapatmak için kullanılan fonksiyon
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
     
-    // Albümümüze ulaşmak için uyguladığımız fonksiyon
+    // Albümümüze ulaşmak için kullanılan fonksiyon
     @objc func addPhoto() {
-        let picker = UIImagePickerController() // "UIImagePickerController" sınıfımızı oluşturuyoruz
+        let picker = UIImagePickerController()
         picker.delegate = self
-        picker.sourceType = .photoLibrary // ulaştığımız yerin tipini seçiyouz kamera albüm gibi
-        picker.allowsEditing = true // fotoğrafı editlemek istiyoruz
-        present(picker, animated: true) // alertlerde yaptığımız şeyin aynısı pickerı gösteriyoruz
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true)
     }
     
     // Kullanıcının fotoğraf seçme işlemi tamamlandığında bu fonksiyon çağrılır.
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        imageView.image = info[.originalImage] as? UIImage // Seçilen fotoğrafı imageview in nesnesi olarak ayarla
-        self.dismiss(animated: true, completion: nil) // Fotoğraf seçim ekranını kapat
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
     
+    // MARK: - Save Button Save CoreData 
+
     @IBAction func saveButton(_ sender: Any) {
-        let appDelagate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelagate.persistentContainer.viewContext
+        // 1. AppDelegate'e erişim sağlar
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
         
+        // 2. CoreData için bir veri bağlamı (context) oluşturur
+        let context = appDelegate.persistentContainer.viewContext
+        
+        // 3. Yeni bir "Books" (Kitaplar) varlığı oluşturur ve bağlama ekler
         let newBook = NSEntityDescription.insertNewObject(forEntityName: "Books", into: context)
+        
+        // 4. Kitabın adını ve yazarını girilen metin alanlarından alır ve varlığa ekler
         newBook.setValue(nameText.text, forKey: "name")
         newBook.setValue(writerText.text, forKey: "writer")
         
-        if let pages = Int(pagesText.text!){
+        // 5. Sayfa sayısını metinden bir tamsayıya dönüştürür ve varlığa ekler
+        if let pages = Int(pagesText.text!) {
             newBook.setValue(pages, forKey: "pages")
         } else {
-            print("Pages Kısmına yanlış bir giriş yapıldı  ")
+            print("Pages Kısmına yanlış bir giriş yapıldı")
         }
-         
+        
+        // 6. Varlığa benzersiz bir kimlik (UUID) ekler
         newBook.setValue(UUID(), forKey: "id")
         
-        let data = imageView.image?.jpegData(compressionQuality: 0.5 )
+        // 7. Görseli JPEG veri biçimine dönüştürür ve varlığa ekler
+        let data = imageView.image?.jpegData(compressionQuality: 0.5)
         newBook.setValue(data, forKey: "image")
         
-        do{
+        // 8. Değişiklikleri kaydeder
+        do {
             try context.save()
-            print("Succes")
+            print("Başarılı")
             print(newBook)
             print(data!)
         } catch {
-            print("Error")
+            print("Hata")
         }
-        
-       
     }
 }
--- Project 15- LibraryApp/Project 15- LibraryApp/ViewController.swift
+++ Project 15- LibraryApp/Project 15- LibraryApp/ViewController.swift
@@ -6,19 +6,56 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var tableView: UITableView!
+    var nameArray = [String]()
+    var idArray = [UUID]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButton))
+        // 1. TableView için veri kaynağı ve delegeleri ayarlar
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // 2. Navigasyon çubuğunda "Ekle" düğmesini ekler
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButton))
     }
-
+    
+    // 3. Veritabanından kitap verilerini almak için kullanılan fonksiyon
+    func getData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Books")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do{
+            let results = try context.fetch(fetchRequest)
+            
+            for result in results as! [NSManagedObject] {
+                print(result)
+            }
+        }catch{
+            print("Hata")
+        }
+    }
+    
+    // 4. "Ekle" düğmesine tıklanınca çalışan fonksiyon
     @objc func addButton() {
         performSegue(withIdentifier: "ToDetailsVC", sender: nil)
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10 // TableView'da toplam 10 satır olacak
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell() // Her hücreyi oluşturur
+        cell.textLabel?.text = "Test" // Her hücrenin metni "Test" olarak ayarlar
+        return cell
+    }
 }
-
