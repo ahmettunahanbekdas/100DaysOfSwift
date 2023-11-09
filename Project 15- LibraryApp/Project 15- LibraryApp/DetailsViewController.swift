aaf3200404da961485fb28b1fd8507ce7b2320f2
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-11-09 06:48:30 +0300

    delete coreData

--- Project 15- LibraryApp/Project 15- LibraryApp/DetailsViewController.swift
+++ Project 15- LibraryApp/Project 15- LibraryApp/DetailsViewController.swift
@@ -10,8 +10,48 @@ class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, 
     @IBOutlet weak var writerText: UITextField!
     @IBOutlet weak var pagesText: UITextField!
     
+    var chosenBook = ""
+    var chosenID : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if chosenBook != ""{
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Books")
+            fetchRequest.returnsObjectsAsFaults = false
+            let idString = chosenID?.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString!)
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        if let name = result.value(forKey: "name") as? String {
+                            nameText.text = name
+                        }
+                        if let writer = result.value(forKey: "writer") as? String   {
+                            writerText.text = writer
+                        }
+                        if let pages = result.value(forKey: "pages") as? Int   {
+                            pagesText.text = String(pages)
+                        }
+                        if let image = result.value(forKey: "image") as?  Data   {
+                            imageView.image = UIImage(data:image)
+                        }
+                    }
+                }
+             }
+            catch {
+                print("Error")
+            }
+        }else{
+            
+        }
+        
+        
         
         // MARK: - Recognizer
         // Klavye kapatma alanı için bir jest tanımlar
@@ -51,31 +91,24 @@ class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, 
     @IBAction func saveButton(_ sender: Any) {
         // 1. AppDelegate'e erişim sağlar
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        
         // 2. CoreData için bir veri bağlamı (context) oluşturur
         let context = appDelegate.persistentContainer.viewContext
-        
         // 3. Yeni bir "Books" (Kitaplar) varlığı oluşturur ve bağlama ekler
         let newBook = NSEntityDescription.insertNewObject(forEntityName: "Books", into: context)
-        
         // 4. Kitabın adını ve yazarını girilen metin alanlarından alır ve varlığa ekler
         newBook.setValue(nameText.text, forKey: "name")
         newBook.setValue(writerText.text, forKey: "writer")
-        
         // 5. Sayfa sayısını metinden bir tamsayıya dönüştürür ve varlığa ekler
         if let pages = Int(pagesText.text!) {
             newBook.setValue(pages, forKey: "pages")
         } else {
             print("Pages Kısmına yanlış bir giriş yapıldı")
         }
-        
         // 6. Varlığa benzersiz bir kimlik (UUID) ekler
         newBook.setValue(UUID(), forKey: "id")
-        
         // 7. Görseli JPEG veri biçimine dönüştürür ve varlığa ekler
         let data = imageView.image?.jpegData(compressionQuality: 0.5)
         newBook.setValue(data, forKey: "image")
-        
         // 8. Değişiklikleri kaydeder
         do {
             try context.save()
@@ -85,5 +118,10 @@ class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, 
         } catch {
             print("Hata")
         }
+        
+        NotificationCenter.default.post(name: NSNotification.Name("Created New Object"), object: nil)
+        
+        self.navigationController?.popViewController(animated: true)
+        
     }
 }
--- Project 15- LibraryApp/Project 15- LibraryApp/ViewController.swift
+++ Project 15- LibraryApp/Project 15- LibraryApp/ViewController.swift
@@ -11,11 +11,21 @@ import CoreData
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
     var nameArray = [String]()
     var idArray = [UUID]()
     
+    var selectedBook = ""
+    var selectedID : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if selectedBook != "" {
+            // CoreData
+        }else {
+            
+        }
         
         // 1. TableView için veri kaynağı ve delegeleri ayarlar
         tableView.delegate = self
@@ -23,39 +33,121 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         // 2. Navigasyon çubuğunda "Ekle" düğmesini ekler
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButton))
+        
+        getData()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name(rawValue: "Created New Object"), object: nil)
+    }
+    
+    
+    
     // 3. Veritabanından kitap verilerini almak için kullanılan fonksiyon
-    func getData() {
+    @objc func getData() {
+        
+        nameArray.removeAll(keepingCapacity: false) // Eski verileri temizle
+        idArray.removeAll(keepingCapacity: false)   // Eski verileri temizle
+        
+        
+        
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Books")
-        fetchRequest.returnsObjectsAsFaults = false
+        fetchRequest.returnsObjectsAsFaults = false // Her zaman kullanmak zorunda değiliz büyük verilerde kullanmak daha verimli olacaktır
         
         do{
             let results = try context.fetch(fetchRequest)
-            
-            for result in results as! [NSManagedObject] {
-                print(result)
+            if  results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let name = result.value(forKey: "name") as? String {
+                        nameArray.append(name)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID   {
+                        idArray.append(id)
+                    }
+                    self.tableView.reloadData()
+                }
             }
         }catch{
             print("Hata")
         }
     }
     
+    
+    
     // 4. "Ekle" düğmesine tıklanınca çalışan fonksiyon
     @objc func addButton() {
-        performSegue(withIdentifier: "ToDetailsVC", sender: nil)
+        selectedBook = ""
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10 // TableView'da toplam 10 satır olacak
+        return nameArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell() // Her hücreyi oluşturur
-        cell.textLabel?.text = "Test" // Her hücrenin metni "Test" olarak ayarlar
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
         return cell
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destination = segue.destination as! DetailsViewController
+            destination.chosenBook = selectedBook
+            destination.chosenID = selectedID
+        }
+        
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedBook = nameArray[indexPath.row]
+        selectedID = idArray[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+        
+    }
+
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>.init(entityName: "Books")
+            
+            let idString = idArray[indexPath.row].uuidString
+            
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject]{
+                        
+                        if let id = result.value(forKey: "id") as? UUID {
+                            if id == idArray[indexPath.row] {
+                                context.delete(result)
+                                nameArray.remove(at: indexPath.row)
+                                idArray.remove(at: indexPath.row)
+                                tableView.reloadData()
+                                
+                                do{
+                                    try context.save()
+                                } catch {
+                                    print("Error ")
+                                }
+                                break
+                            }
+                        }
+                    }
+                }
+            } catch {
+                print("Error")
+            }
+        }
+    }
+    
 }
