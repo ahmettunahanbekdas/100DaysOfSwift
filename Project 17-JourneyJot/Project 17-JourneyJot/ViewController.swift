8b3637a1d8fc5275e0985de4b08ff46325e5ba84
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-11-12 18:59:59 +0300

    the user's location was shown on the map

--- Project 17-JourneyJot/Project 17-JourneyJot/ViewController.swift
+++ Project 17-JourneyJot/Project 17-JourneyJot/ViewController.swift
@@ -9,26 +9,35 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
-
+    
     @IBOutlet weak var mapView: MKMapView!
     
     var locationManager = CLLocationManager() // Lokasyon kontrolü için obje oluşturduk
-    
-     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mapView.delegate = self // mapView delegate ekledik
-        locationManager.delegate = self // localtionManager delegate ekledik
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest // hangi keskinlikte konumu istediğimi belirtiriz keskinlik arttıkça pil kullanımı artar
-        locationManager.requestWhenInUseAuthorization() // Ne sıklıkla kullanıcının konumunu istiyoruz burada uygulamayı kullandığı esnada konum bilgisi alınır
-        locationManager.startUpdatingLocation() // Kullanıcının konumunu almaya başladık
-
+        mapView.delegate = self // Harita işlemleri için delegate'i ViewController'a atıyoruz
+        locationManager.delegate = self // Konum işlemleri için delegate'i ViewController'a atıyoruz
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // Konumun hassasiyetini belirtiyoruz; en yüksek hassasiyeti seçtik, pil tüketimi artabilir
+        locationManager.requestWhenInUseAuthorization() // Uygulama kullanılırken konum bilgisine ihtiyaç duyduğumuzu belirtiyoruz
+        locationManager.startUpdatingLocation() // Kullanıcının konumunu almaya başlıyoruz
     }
     
-
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locations = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        // Yeni konum bilgisini alıp, CLLocationCoordinate2D formatına dönüştürüyoruz
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        // MapView'da gösterilecek bölgenin genişliğini ve yüksekliğini belirlemek için bir MKCoordinateSpan oluşturuyoruz
+        // Burada 0.1 değerleri, görüntülenen bölgenin enlem ve boylamındaki farklılık aralıklarını temsil eder
+        
+        let region = MKCoordinateRegion(center: locations, span: span)
+        // MKCoordinateRegion, belirtilen konum ve görüntüleme aralığını birleştirerek bir bölge oluşturur
+    
+        mapView.setRegion(region, animated: true)
+        // Oluşturduğumuz bölgeyi haritaya setRegion fonksiyonu ile belirterek haritada o bölgenin gösterilmesini sağlarız
+    }
 }
 
