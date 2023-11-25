d41da49a2c46ca12e54053582636b8cada46569f
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-11-25 23:13:56 +0300

    create listviewcontroller

---
+++ Project 17-JourneyJot/Project 17-JourneyJot/ListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ListViewController.swift
+//  Project 17-JourneyJot
+//
+//  Created by Ahmet Tunahan Bekdaş on 25.11.2023.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
