0bf211db85eedb18fa8ab5bd1822d7cd4d80534a
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-11-28 10:43:02 +0300

    show data

--- Project 17- JourneyJot/Project 17- JourneyJot/ListViewController.swift
+++ Project 17- JourneyJot/Project 17- JourneyJot/ListViewController.swift
@@ -6,37 +6,73 @@
 //
 
 import UIKit
+import CoreData
 
 class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     
     
     @IBOutlet weak var tableView: UITableView!
+    var nameArray = [String]()
+    var idArray = [UUID]()
+ 
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButton))
         
         tableView.delegate = self
         tableView.dataSource = self
         
+        getData()
+        
     }
     
     // MARK: - Funcitons
     
-    @objc func addButton(){
+    
+    func getData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
         
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do{
+            let results = try context.fetch(fetchRequest)
+            if results.count > 0 {
+                
+                self.nameArray.removeAll(keepingCapacity: false)
+                self.idArray.removeAll(keepingCapacity: false)
+                
+                for result in results as! [NSManagedObject] {
+                    if let name = result.value(forKey: "name") as? String{
+                        nameArray.append(name)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID{
+                        idArray.append(id)
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+        }catch{
+            print("Error")
+        }
     }
-
+    
+    @objc func addButton(){
+        performSegue(withIdentifier: "toViewController", sender: nil)
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return nameArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Test"
+        cell.textLabel?.text = nameArray[indexPath.row]
         return cell
     }
-
 }
