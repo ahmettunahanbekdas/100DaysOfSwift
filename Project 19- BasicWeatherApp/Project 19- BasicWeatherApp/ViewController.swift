5dbd3b62b22874f631589ff6ee372ca4427fccca
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-12-01 14:42:31 +0300

    get user locations

--- Project 19- BasicWeatherApp/Project 19- BasicWeatherApp/ViewController.swift
+++ Project 19- BasicWeatherApp/Project 19- BasicWeatherApp/ViewController.swift
@@ -1,75 +1,85 @@
-//
-//  ViewController.swift
-//  Project 19- BasicWeatherApp
-//
-//  Created by Ahmet Tunahan Bekdaş on 30.11.2023.
-//
-
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var feelLabel: UILabel!
     @IBOutlet weak var windyLabel: UILabel!
+    var locationManager = CLLocationManager()
     var api = "8fd636941fefae14223ab9d35b83ac5c"
-    
+    var latitude = ""
+    var logitude = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Weather API"
+        title = "Basic Weather App"
+
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        
+        
     }
-    
-    // Request & Session = Veriyi alacağımız bağlantıya gitme işlemi
-    // Response & Data = Burada ki Response veya Datayı alma işlemi
-    // Parsing & JSON Serialization = Datayı işleme işlemi
-    
+
     @IBAction func getButton(_ sender: Any) {
-        
         // 1.Request & Session
-        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=41.11315928467933&lon=29.021466229358197&appid=\(api)")
-        
-        let session = URLSession.shared
-        
-        let task = session.dataTask(with: url!) { data, response, error in
-            if error != nil {
-                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-                let okButton = UIAlertAction(title: "Okay", style: UIAlertAction.Style.default)
-                alert.addAction(okButton)
-                self.present(alert, animated: true)
-            }else{
-                // 2.Response & Data
-                if  let data = data {
-                    do{
-                        let jsonResponse = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? [String:Any]
-                        
-                        DispatchQueue.main.async {
-                            if let main = jsonResponse!["main"] as? [String:Any]{
-                                if let temp = main["temp"] as? Double{
-                                    self.tempLabel.text = String(temp)
-                                    print(temp)
+        if let location = locationManager.location {
+            let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(location.coordinate.latitude)&lon=\(location.coordinate.longitude)&appid=\(api)")
+            
+            let session = URLSession.shared
+            
+            let task = session.dataTask(with: url!) { data, response, error in
+                if error != nil {
+                    let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let okButton = UIAlertAction(title: "Okay", style: UIAlertAction.Style.default)
+                    alert.addAction(okButton)
+                    self.present(alert, animated: true)
+                } else {
+                    // 2.Response & Data
+                    if let data = data {
+                        do {
+                            let jsonResponse = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? [String:Any]
+                            
+                            DispatchQueue.main.async {
+                                if let main = jsonResponse?["main"] as? [String:Any] {
+                                    if let temp = main["temp"] as? Double {
+                                        self.tempLabel.text = String(temp)
+                                    }
+                                    if let feelsTemp = main["feels_like"] as? Double {
+                                        self.feelLabel.text = String(feelsTemp)
+                                    }
                                 }
-                                if let feelsTemp = main["feels_like"] as? Double{
-                                    self.feelLabel.text = String(feelsTemp)
+                                if let wind = jsonResponse?["wind"] as? [String:Any] {
+                                    if let windy = wind["speed"] as? Double {
+                                        self.windyLabel.text = String(windy)
+                                    }
                                 }
                             }
-                            if let wind = jsonResponse!["wind"] as? [String:Any]{
-                                if let windy = wind["speed"] as? Double{
-                                    self.windyLabel.text = String(windy)
-                                }
-                            }
+                        } catch {
+                            print("Error Response")
                         }
-                    }catch{
-                        print("Error Response")
                     }
-                    
                 }
             }
+            task.resume()
+        } else {
+            print("Location not available yet.")
         }
-        task.resume()
     }
-    
+
+   
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            // Kullanıcının son konumu
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+
+            
+            
+        }
+    }
 }
-
-
-
 
