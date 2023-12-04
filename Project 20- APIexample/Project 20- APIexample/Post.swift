e3f726cc59a7f1ddcbfb842d74a6eca95786013f
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-12-04 18:44:39 +0300

    project 20 added

---
+++ Project 20- APIexample/Project 20- APIexample/Post.swift
@@ -0,0 +1,16 @@
+//
+//  Post.swift
+//  Project 20- APIexample
+//
+//  Created by Ahmet Tunahan Bekdaş on 2.12.2023.
+//
+
+import Foundation
+
+struct Post: Codable {
+    var userId: Int
+    var id: Int
+    var title: String
+    var body: String
+    
+}
