d40d74dcc5c3c374e49c2cdc3fa32c37ae0b8a4e
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-12-04 18:22:01 +0300

    added main page tabbar and signin, singup page models

--- Project 21- BasicsInstagramClone/Project 21- BasicsInstagramClone/ViewController.swift
+++ Project 21- BasicsInstagramClone/Project 21- BasicsInstagramClone/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInButton(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedViewController", sender: nil)
+    }
+    
+    @IBAction func signUpPassword(_ sender: Any) {
+    }
 }
 
