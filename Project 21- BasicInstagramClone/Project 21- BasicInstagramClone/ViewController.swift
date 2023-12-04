161fabfa53f23472dbc919bb01267dc680d7d9b5
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-12-05 01:26:54 +0300

    user signup

--- Project 21- BasicInstagramClone/Project 21- BasicInstagramClone/ViewController.swift
+++ Project 21- BasicInstagramClone/Project 21- BasicInstagramClone/ViewController.swift
@@ -18,12 +18,31 @@ class ViewController: UIViewController {
     }
 
     @IBAction func signInButton(_ sender: Any) {
-        performSegue(withIdentifier: "toFeedViewController", sender: nil)
+        
         
     }
     
     
     @IBAction func signUpButton(_ sender: Any) {
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { authData, error in
+                 
+                if error != nil {
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                }else{
+                    self.performSegue(withIdentifier: "toFeedViewController", sender: nil)
+                }
+            }
+        }else{
+            makeAlert(title: "Error", message: "Please Enter Username and Password")
+        }
+    }
+    
+    func makeAlert(title: String, message:String) {
+        let alert = UIAlertController(title: title, message: message , preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Okay", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
