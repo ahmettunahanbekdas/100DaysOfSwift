b44bc96bb11023bc80c3446aaba40019dd30516c
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-12-06 16:29:32 +0300

    change upload data and add date

--- Project 21- BasicInstagramClone/Project 21- BasicInstagramClone/UploadViewController.swift
+++ Project 21- BasicInstagramClone/Project 21- BasicInstagramClone/UploadViewController.swift
@@ -51,44 +51,63 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate , 
     //MARK: - shareButton
     
     @IBAction func shareButton(_ sender: Any) {
+        guard let selectedImage = imageView.image else {
+            makeAlert(title: "Error", message: "Please Choose Image")
+            return
+        }
         
         let storage = Storage.storage()
-        let storageReferance = storage.reference()
+        let storageReference = storage.reference()
         let uuid = UUID().uuidString
         
-        
-        let mediaFolder = storageReferance.child("media")
+        let mediaFolder = storageReference.child("media")
         let imageReference = mediaFolder.child("\(uuid).jpg")
         
-        if let data = imageView.image?.jpegData(compressionQuality: 0.5){
-            
-            imageReference.putData(data, metadata: nil) { metaData, error in
+        if let imageData = selectedImage.jpegData(compressionQuality: 0.5) {
+            imageReference.putData(imageData, metadata: nil) { _, error in
+                if let error = error {
+                    self.makeAlert(title: "Error", message: error.localizedDescription)
+                    return
+                }
                 
-                if  error == nil {
-                    
-                    imageReference.downloadURL { url, error in
-                        if error == nil {
-                            
-                            let imageUrl = url?.absoluteString
-                            
-                            
-                            // Firestore Database
-                            
-                            let firestoreDatabase = Firestore.firestore()
-                            var firesotreReferance: DocumentReference
-                            
-                            let firestorePosts = ["imageUrl" : imageUrl!, "postBy" : Auth.auth().currentUser!.email!, "postComment" : self.commentTextField.text! , "date":"date", "like":0] as [String:Any]
-                            
-                            
-                            
-                            firesotreReferance = firestoreDatabase.collection("Posts").addDocument(data: firestorePosts, completion: { error in
-                                if error !=  nil {
-                                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
-                                }
-                            })
-                        }
-                        
+                imageReference.downloadURL { url, error in
+                    if let error = error {
+                        self.makeAlert(title: "Error", message: error.localizedDescription)
                     }
+                    
+                    guard let imageUrl = url?.absoluteString else {
+                        self.makeAlert(title: "Error", message: "Image URL is nil")
+                        return
+                    }
+                    
+                                   
+                    if self.commentTextField.text == "" {
+                        self.makeAlert(title: "Error", message: "Please Enter Comment")
+                        return
+                    }
+                    
+                    // Firestore Database
+                    
+                    let firestoreDatabase = Firestore.firestore()
+                    var firestoreReferance : DocumentReference?
+                    
+                    let firestorePost = [
+                        "imageUrl": imageUrl,
+                        "postBy": Auth.auth().currentUser?.email ?? "",
+                        "postComment": self.commentTextField.text ?? "",
+                        "date": FieldValue.serverTimestamp(),
+                        "like": 0
+                    ]
+                    
+                    firestoreReferance = firestoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: { error in
+                        if error != nil {
+                            self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                        }
+                    })
+                    self.makeAlert(title: "Succes", message: "")
+                    self.imageView.image = UIImage(named: "addPhoto")
+                    self.commentTextField.text = ""
+                    self.tabBarController?.selectedIndex = 0
                 }
             }
         }
