c022a61aa79ae7aa2be63ae4145022ab84d142be
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-12-13 13:55:38 +0300

    created project 22 foursquareClone

---
+++ Atıl Samancıoğlu | iOS Swift Mobil Uygulama Geliştirme | Udemy/Project 22- FoursquareClone/Project 22- FoursquareClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 22- FoursquareClone
+//
+//  Created by Ahmet Tunahan Bekdaş on 13.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
